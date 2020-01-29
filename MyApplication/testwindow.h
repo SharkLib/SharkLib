834cd7ef6d23aa22c38ec58a474943e493b93f54
rong huang <sharversion@gmail.com>
2020-01-28 20:32:46 -0500

    Init Lib

---
+++ MyApplication/testwindow.h
@@ -0,0 +1,27 @@
+#ifndef TESTWINDOW_H
+#define TESTWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class TestWindow; }
+QT_END_NAMESPACE
+
+class TestWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	TestWindow(QWidget *parent = nullptr);
+	~TestWindow();
+
+	void loadPlugins();
+	void populateMenus(QObject *plugin);
+
+public slots:
+	void slot_activeWindow();
+
+private:
+	Ui::TestWindow *ui;
+};
+#endif // TESTWINDOW_H
